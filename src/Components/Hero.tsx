f1861643d85c8e5bfa1f732e84829abeef6a6454
MD Titumir Anan <titumiranan.gtc@gmail.com>
2024-03-13 12:00:57 +0600

    update

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,36 +1,75 @@
-import Link from 'next/link';
-import React from 'react';
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
 
 const Hero = () => {
-    return (
-        <div className='' >
-            <nav className='text-white flex justify-between items-center'>
-                <div>
-                    Titumir Anan
-                </div>
-                <div className='flex gap-5 justify-center items-center'>
-                    <Link href={'/'}>WORKS</Link>
-                    <Link href={'/about'}>ABOUT</Link>
-                    <button className='btn'>Say Hello</button>
-                </div>
-            </nav>
-            {/* heading content  */}
-            <div className='mt-[141px] flex justify-center items-end gap-5 '>
-                <div>
-                    <div className='font-bold text-[88px]'>
-                        <span className='text-[#919BBA] '>Turning </span> <span className='text-[#F6F7FA]'>Code</span> <span className='text-[#919BBA] '>Into</span><br /> <span className='text-[#919BBA]'>Creative </span> <span className='text-[#F6F7FA]'>Solutions</span>
-                    </div>
-                </div>
-                <div >
-                    <p className='lg:w-[378px] lg:h-[78px] text-[#F6F7FA] mb-5'>Empowering the digital landscape with precision and passion, transforming ideas into immersive online experiences.</p>
-                </div>
-            </div>
-            <div className='flex justify-center items-center gap-10'>
-                <button className='text-white border rounded-s-full  rounded-e-full px-[39px] py-[20px] hover:bg-[#6919FF] text-[16px] font-[500]'>Explore My Works</button>
-                <button className='text-white border rounded-s-full  rounded-e-full px-[39px] py-[20px] hover:bg-[#6919FF] text-[16px] font-[500]'> Download CV </button>
-            </div>
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="">
+      <nav className="text-white flex justify-between items-center">
+        <div className="">
+          <span className="font-bold inter uppercase text-white inter text-[24px] leading-[29px]">
+            Titumir &nbsp;
+          </span>
+          <span className="font-[400px] inter text-[24px] leading-[29px] uppercase">
+            ANAN
+          </span>
         </div>
-    );
+        <div className="md:flex gap-5 justify-center items-center hidden">
+          <Link
+            className="inter font-bold text-[16px] leading-[19.36px]"
+            href={"/"}
+          >
+            WORKS
+          </Link>
+          <Link
+            className="inter font-bold text-[16px] leading-[19.36px]"
+            href={"/about"}
+          >
+            ABOUT
+          </Link>
+          <button className="btn inter font-[500] text-[16px] leading-[19.36px]">
+            Say Hello
+          </button>
+        </div>
+        <div className="md:hidden ">
+          <div
+            className="flex flex-col gap-2 group "
+            onClick={() => setOpen(!open)}
+          >
+            <span className=" w-10 h-[3px] bg-white border border-white text-black flex"></span>
+            <span className=" w-10 h-[3px] bg-white border border-white text-black flex"></span>
+            <span className=" w-10 h-[3px] bg-white border border-white text-black flex"></span>
+          </div>
+        </div>
+      </nav>
+      {/* heading content  */}
+      <div className="mt-[141px]  flex flex-col lg:flex-row  items-center gap-10 lg:gap-5">
+        <div className="font-bold  lg:w-[834px] lg:text-[78px] md:text-[78px] text-[50px] md:leading-[106px] h-[214px]">
+          <span className="text-[#919BBA]   ">Turning </span>{" "}
+          <span className="text-[#F6F7FA]    ">Code</span>{" "}
+          <span className="text-[#919BBA]    ">Into</span>
+          <br className="hidden md:block" />
+          <span className="text-[#919BBA]    ">Creative </span>{" "}
+          <span className="text-[#F6F7FA]    ">Solutions</span>
+        </div>
+        <div>
+          <p className="lg:w-[387px] flex lg:mt-28 lg:h-[78px] inter font-[500] text-[16px] text-[#F6F7FA] mb-5">
+            Empowering the digital landscape with precision and passion,
+            transforming ideas into immersive online experiences.
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-center md:flex-row flex-col mt-[40px] items-center gap-10  md:mt-[75px]">
+        <button className="text-white border rounded-s-full  rounded-e-full px-[39px] py-[20px] hover:bg-[#6919FF] text-[16px] font-[500]">
+          Explore My Works
+        </button>
+        <button className="text-white border rounded-s-full  rounded-e-full px-[39px] py-[20px] hover:bg-[#6919FF] text-[16px] font-[500]">
+          Download CV
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import About from '@/Components/About';
-import Exprience from '@/Components/Exprience/Exprience';
-import Footer from '@/Components/Footer';
-import Hero from '@/Components/Hero';
-import Workconatact from '@/Components/Work Contact/Workconatact';
-import Work from '@/Components/work/Work';
-import React from 'react';
+import About from "@/Components/About";
+import Exprience from "@/Components/Exprience/Exprience";
+import Footer from "@/Components/Footer";
+import Hero from "@/Components/Hero";
+import Workconatact from "@/Components/Work Contact/Workconatact";
+import Work from "@/Components/work/Work";
+import React from "react";
 
 const Home = () => {
   return (
-    <div className='bg-[#060918] max-w-[1440px]   mx-auto padding'>
+    <div className=" lg:max-w-[1200px] lg:px-0 px-10 pt-10  mx-auto padding">
       <Hero />
       <About />
       <Work />
